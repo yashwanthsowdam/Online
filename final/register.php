9e8afb5cad586b1147f02b99aa76155a9d640a54
Yashwanth Sowdam <yashwanth.sowdam22@gmail.com>
2017-04-13 02:44:04 +1000

    First
    Commit

---
+++ final/register.php
@@ -0,0 +1,233 @@
+<?php
+	ob_start();
+	session_start();
+	if( isset($_SESSION['user'])!="" ){
+		header("Location: home.php");
+	}
+	include_once 'dbconnect.php';
+
+	$error = false;
+
+	if ( isset($_POST['btn-signup']) ) {
+		
+		// clean user inputs to prevent sql injections
+		$name = trim($_POST['name']);
+		$name = strip_tags($name);
+		$name = htmlspecialchars($name);
+		
+		$email = trim($_POST['email']);
+		$email = strip_tags($email);
+		$email = htmlspecialchars($email);
+		
+		$pass = trim($_POST['pass']);
+		$pass = strip_tags($pass);
+		$pass = htmlspecialchars($pass);
+		
+		// basic name validation
+		if (empty($name)) {
+			$error = true;
+			$nameError = "Please enter your full name.";
+		} else if (strlen($name) < 3) {
+			$error = true;
+			$nameError = "Name must have atleat 3 characters.";
+		} else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+			$error = true;
+			$nameError = "Name must contain alphabets and space.";
+		}
+		
+		//basic email validation
+		if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
+			$error = true;
+			$emailError = "Please enter valid email address.";
+		} else {
+			// check email exist or not
+			$query = "SELECT userEmail FROM users WHERE userEmail='$email'";
+			$result = mysql_query($query);
+			$count = mysql_num_rows($result);
+			if($count!=0){
+				$error = true;
+				$emailError = "Provided Email is already in use.";
+			}
+		}
+		// password validation
+		if (empty($pass)){
+			$error = true;
+			$passError = "Please enter password.";
+		} else if(strlen($pass) < 6) {
+			$error = true;
+			$passError = "Password must have atleast 6 characters.";
+		}
+		
+		// password encrypt using SHA256();
+		$password = hash('sha256', $pass);
+		
+		// if there's no error, continue to signup
+		if( !$error ) {
+			
+			$query = "INSERT INTO users(userName,userEmail,userPass) VALUES('$name','$email','$password')";
+			$res = mysql_query($query);
+				
+			if ($res) {
+				$errTyp = "success";
+				$errMSG = "Successfully registered, you may login now";
+				unset($name);
+				unset($email);
+				unset($pass);
+			} else {
+				$errTyp = "danger";
+				$errMSG = "Something went wrong, try again later...";	
+			}	
+				
+		}
+		
+		
+	}
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+<head>
+  <title>Online Shopping</title>
+  <meta name="description" content="website description" />
+  <meta name="keywords" content="website keywords, website keywords" />
+  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <!-- class="logo_colour", allows you to change the colour of the text -->
+          <h1><a href="page.html">Online<span class="logo_colour">Shopping</span></a></h1>
+          <h2>Style . Fashion </h2>
+        </div>
+      </div>
+      <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li><a href="index.html">Home</a></li>
+          <li><a href="examples.html">Mens</a></li>
+          <li><a href="page.html">Womens</a></li>
+          <li><a href="another_page.html">Kids</a></li>
+          <li><a href="contact.html">Sale</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="site_content">
+      <div class="sidebar">
+        <!-- insert your sidebar items here -->
+        <h3>Latest News</h3>
+        <h4>New Products</h4>
+        <h5>April 10th, 2017</h5>
+        <p>Check out the new limited edition clothing.<br />
+        <p></p>
+        <h4>New Offers available.</h4>
+        <h5>April 10th, 2017</h5>
+        <p>Check out the new offers available for limited time.<br />
+        <h3>Useful Links</h3>
+        <ul>
+          <li><a href="#">Mens Clothing</a></li>
+          <li><a href="#">Womens Clothing</a></li>
+          <li><a href="#">Kids range</a></li>
+          <li><a href="#">Winter clothing</a></li>
+        </ul>
+        <h3>Search</h3>
+        <form method="post" action="#" id="search_form">
+          <p>
+            <input class="search" type="text" name="search_field" value="Enter keywords....." />
+            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+          </p>
+        </form>
+      </div>
+      <div id="content">
+        
+		<div class="container">
+
+	<div id="login-form">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" autocomplete="off">
+    
+    	<div class="col-md-12">
+        
+        	<div class="form-group">
+            	<h2 class="">Sign Up.</h2>
+            </div>
+        
+        	<div class="form-group">
+            	<hr />
+            </div>
+            
+            <?php
+			if ( isset($errMSG) ) {
+				
+				?>
+				<div class="form-group">
+            	<div class="alert alert-<?php echo ($errTyp=="success") ? "success" : $errTyp; ?>">
+				<span class="glyphicon glyphicon-info-sign"></span> <?php echo $errMSG; ?>
+                </div>
+            	</div>
+                <?php
+			}
+			?>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+            	<input type="text" name="name" class="form-control" placeholder="Enter Name" maxlength="50" value="<?php echo $name ?>" />
+                </div>
+                <span class="text-danger"><?php echo $nameError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+            	<input type="email" name="email" class="form-control" placeholder="Enter Your Email" maxlength="40" value="<?php echo $email ?>" />
+                </div>
+                <span class="text-danger"><?php echo $emailError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
+            	<input type="password" name="pass" class="form-control" placeholder="Enter Password" maxlength="15" />
+                </div>
+                <span class="text-danger"><?php echo $passError; ?></span>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<button type="submit" class="btn btn-block btn-primary" name="btn-signup">Sign Up</button>
+            </div>
+            
+            <div class="form-group">
+            	<hr />
+            </div>
+            
+            <div class="form-group">
+            	<a href="index.php">Sign in Here...</a>
+            </div>
+        
+        </div>
+   
+    </form>
+    </div>	
+
+</div>
+
+		
+      </div>
+    </div>
+    <div id="footer">
+      Copyright &copy; 2107Online Shopping | <a href="#">All rights reserved</a> | <a href="#">See our terms of use</a> | <a href="#">Privacy policy</a>
+    </div>
+  </div>
+</body>
+</html>
+
+
+<?php ob_end_flush(); ?>
